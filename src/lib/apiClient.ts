6adb40bb7f2eb60b6290002aa5cae08175ca58bf
uxy0301@gmail.com <umeharakouhei@umeharakouheinoMac-mini.local>
2023-07-17 16:22:49 +0900

    base url

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
